5b8926be83afc2716a721a7afb3829b6501d3eb6
DimkaVeselov <babunovd@mail.ru>
2021-01-18 22:43:11 +0300

    task 19

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import styles from './Footer.module.css'
 
-const Footer = ({ count }) => (<p>Осталось выполнить: { count }</p>);
+const Footer = ({ count }) => (
+  <footer className={styles.wrapFooter}>
+    <p>
+      Осталось: {count}
+    </p>
+
+    <div>
+      <Button color="primary">все</Button>
+      <Button color="primary">активные</Button>
+      <Button color="primary">выполнено</Button>
+    </div>
+
+    <div>
+      <IconButton
+        aria-label="delete"
+        color='primary'>
+        <DeleteIcon />
+      </IconButton>
+    </div>
+
+  </footer>);
 
 export default Footer;
