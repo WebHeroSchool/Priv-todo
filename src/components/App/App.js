a0e1671172ba3aeb824d5cd25b6ea7436be419b5
DimkaVeselov <babunovd@mail.ru>
2021-01-23 02:04:52 +0300

    task 20

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,32 +4,34 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from'./App.module.css';
 
-const App = () => {
+class App extends React.Component {
 
-  const Items = [
-    {
-      value: 'Написать новое приложение',
-      isDone: true
-    },
-    {
-      value: 'Протестировать',
-      isDone: false
-    },
-    {
-      value: 'Запушить',
-      isDone: false
-    }
-  ]
-  
-  return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>Важные дела:</h1>
-      <div className={styles.wrapList}>
-        <InputItem />
-        <ItemList Items={Items} />
-      </div>
-      <Footer count={2} />
-    </div>);
-}
+  render() {
+    const Items = [
+      {
+        value: 'Написать новое приложение',
+        isDone: true
+      },
+      {
+        value: 'Протестировать',
+        isDone: false
+      },
+      {
+        value: 'Запушить',
+        isDone: false
+      }
+    ]
+    
+    return (
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>Важные дела:</h1>
+        <div className={styles.wrapList}>
+          <InputItem />
+          <ItemList Items={Items} />
+        </div>
+        <Footer count={2} />
+      </div>);
+ }
+};
 
 export default App;
